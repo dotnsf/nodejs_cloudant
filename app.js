66508dd24953ce0d7fd3b2f21596512aaf6ffd72
Kei Kimura <dotnsf@jp.ibm.com>
2017-10-25 09:27:12 +0900

    filenames changed.

--- app.js
+++ app.js
@@ -1,31 +1,73 @@
 // app.js
 
+var Cloudantlib = require( 'cloudant' );
 var express = require( 'express' );
 var bodyParser = require( 'body-parser' );
 var app = express();
 
-var data = [];
+var settings = require( './settings' );
+var cloudant = Cloudantlib( { account: settings.cloudant_username, password: settings.cloudant_password } );
+var db = cloudant.db.use( settings.cloudant_db );
 
 app.use( express.static( __dirname + '/public' ) );
 //app.use( bodyParser.urlencoded( { extended: true, limit: '10mb' } ) );
 app.use( bodyParser.urlencoded() );
 app.use( bodyParser.json() );
 
-app.get( '/hello', function( req, res ){
-  res.write( 'Hello World.' );
-  res.end();
-});
-
 app.post( '/mypost', function( req, res ){
-//  console.log( req.body );
-  data.push( req.body );
-  res.write( JSON.stringify( req.body ) );
-  res.end();
+  console.log( 'POST /mypost' );
+  console.log( req.body );
+  db.insert( req.body, function( err, body, header ){
+    if( err ){
+      res.status( 400 );
+      res.write( JSON.stringify( { status: false, message: err }, 2, null ) );
+      res.end();
+    }else{
+      res.write( JSON.stringify( { status: true, doc: req.body }, 2, null ) );
+      res.end();
+    }
+  });
 });
 
 app.get( '/mydata', function( req, res ){
-  res.write( JSON.stringify( data ) );
-  res.end();
+  console.log( 'GET /mydata' );
+  db.list( { include_docs: true }, function( err, body ){
+    if( err ){
+      res.status( 400 );
+      res.write( JSON.stringify( { status: false, message: err }, 2, null ) );
+      res.end();
+    }else{
+      var docs = [];
+      for( var i = 0; i < body.rows.length; i ++ ){
+        docs.push( body.rows[i].doc );
+      }
+      res.write( JSON.stringify( { status: true, docs: docs }, 2, null ) );
+      res.end();
+    }
+  });
+});
+
+app.delete( '/mydelete/:id', function( req, res ){
+  var id = req.params.id;
+  console.log( 'DELETE /mydelete/' + id );
+  db.get( id, function( err, data ){
+    if( err ){
+      res.status( 400 );
+      res.write( JSON.stringify( { status: false, message: err }, 2, null ) );
+      res.end();
+    }else{
+      db.destroy( id, data._rev, function( err, body ){
+        if( err ){
+          res.status( 400 );
+          res.write( JSON.stringify( { status: false, message: err }, 2, null ) );
+          res.end();
+        }else{
+          res.write( JSON.stringify( { status: true }, 2, null ) );
+          res.end();
+        }
+      });
+    }
+  });
 });
 
 var port = 3000;
--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-exports.cloudant_username = 'cloudant_username';
-exports.cloudant_password = 'cloudant_password';
-exports.cloudant_db = 'testdb';
+exports.cloudant_username = '74b8201c-dd3d-42d0-8cd2-81f4085f2aac-bluemix';
+exports.cloudant_password = 'b3b0fbbd8a6a217cf1468c82ad452ca408f64b6075764a121c10cb05f6f53bd8';
+exports.cloudant_db = 'testdb1023';
